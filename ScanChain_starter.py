2462b9f1a773aff28dabfb4b6ea1830d79b3153a
jessica-chann <jchan4@andrew.cmu.edu>
2025-03-28 11:23:46 -0400

    task1

--- ScanChain_starter.py
+++ ScanChain_starter.py
@@ -7,7 +7,7 @@ from cocotb.triggers import Timer
 # to the filepath of the .log
 # file you are working with
 CHAIN_LENGTH = -1
-FILE_NAME    = ""
+FILE_NAME    = "adder/adder.log"
 
 
 
@@ -126,7 +126,17 @@ async def step_clock(dut):
     # TODO: YOUR CODE HERE 
     ######################
 
-    pass
+    # Set clock high
+    dut.clk.value = 1
+    
+    # Wait for 10 ns
+    await Timer(10, units='ns')
+    
+    # Set clock low
+    dut.clk.value = 0
+    
+    # Wait for 10 more ns
+    await Timer(10, units='ns')
     
 
 #-------------------------------------------------------------------
@@ -142,8 +152,20 @@ async def input_chain_single(dut, bit, ff_index):
     # TODO: YOUR CODE HERE 
     ######################
 
-    pass
+    # Enable scan mode
+    dut.scan_en.value = 1
     
+    # Shift the bit to the correct position in the chain
+    for i in range(ff_index):
+        dut.scan_in.value = 0
+        await step_clock(dut)
+    
+    # Insert target bit
+    dut.scan_in.value = bit
+    await step_clock(dut)
+    
+    # Disable scan mode when done
+    dut.scan_en.value = 0
 #-------------------------------------------------------------------
 
 # This function places multiple bit values inside FFs of specified indexes.
@@ -159,7 +181,21 @@ async def input_chain(dut, bit_list, ff_index):
     # TODO: YOUR CODE HERE 
     ######################
 
-    pass
+    # Enable scan mode
+    dut.scan_en.value = 1
+    
+    # Put the first bit at position ff_index
+    for i in range(ff_index):
+        dut.scan_in.value = 0
+        await step_clock(dut)
+    
+    # Insert list of bits in reverse order
+    for bit in reversed(bit_list):
+        dut.scan_in.value = bit
+        await step_clock(dut)
+    
+    # Disable scan mode when done
+    dut.scan_en.value = 0
 
 #-----------------------------------------------
 
@@ -172,7 +208,22 @@ async def output_chain_single(dut, ff_index):
     # TODO: YOUR CODE HERE 
     ######################
 
-    pass       
+    # Enable scan mode
+    dut.scan_en.value = 1
+    
+    
+    shifts_needed = CHAIN_LENGTH - ff_index - 1
+    result = None
+    
+    for i in range(CHAIN_LENGTH):
+        await step_clock(dut)
+        if i == shifts_needed:
+            result = int(dut.scan_out.value)
+    
+    # Disable scan mode when done
+    dut.scan_en.value = 0
+    
+    return result 
 
 #-----------------------------------------------
 
@@ -187,7 +238,23 @@ async def output_chain(dut, ff_index, output_length):
     # TODO: YOUR CODE HERE 
     ######################
 
-    pass       
+    # Enable scan mode
+    dut.scan_en.value = 1
+
+    shifts_needed = CHAIN_LENGTH - (ff_index + output_length - 1) - 1
+    result = []
+    
+    # Shift out all bits
+    for i in range(CHAIN_LENGTH):
+        await step_clock(dut)
+        bit_position = i - shifts_needed
+        if 0 <= bit_position < output_length:
+            result.append(int(dut.scan_out.value))
+    
+    # Disable scan mode when done
+    dut.scan_en.value = 0
+    
+    return result
 
 #-----------------------------------------------
 
@@ -206,4 +273,70 @@ async def test(dut):
     ######################
     # TODO: YOUR CODE HERE 
     ######################
+    
+    CHAIN_LENGTH = chain.chain_length
+    
+    # Print chain info for debugging
+    print_chain(chain)
+    
+    # Initialize circuit
+    dut.scan_en.value = 0
+    dut.scan_in.value = 0
+    dut.clk.value = 0
+    
+    # Test different input values for the adder
+    test_cases = [
+        (0b0101, 0b0011),  # 5 + 3 = 8
+        (0b1010, 0b0011),  # 10 + 3 = 13
+        (0b1111, 0b0001)   # 15 + 1 = 16
+    ]
+    
+    for a_val, b_val in test_cases:
+        # Convert inputs to bit lists
+        a_bits = [int(bit) for bit in bin(a_val)[2:].zfill(4)]
+        b_bits = [int(bit) for bit in bin(b_val)[2:].zfill(4)]
+        
+        # Create list of all bits to scan in
+        # Order matters: need to set all registers in a single scan chain pass
+        # Need to identify register positions from the chain object
+        
+        # Access register indices from the chain
+        a_reg = chain.registers["a_reg"]
+        b_reg = chain.registers["b_reg"]
+        x_out = chain.registers["x_out"]
+        
+        # Prepare all zeroes first
+        all_bits = [0] * CHAIN_LENGTH
+        
+        # Set input bits in the right positions
+        # Careful with bit ordering here (MSB vs LSB)
+        for i in range(a_reg.size):
+            all_bits[a_reg.index_list[i]] = a_bits[i]
+        
+        for i in range(b_reg.size):
+            all_bits[b_reg.index_list[i]] = b_bits[i]
+        
+        # Scan in all values at once
+        # Since we're setting all bits, we start at index 0
+        await input_chain(dut, all_bits, 0)
+        
+        # Disable scan mode and pulse the clock to execute the addition
+        dut.scan_en.value = 0
+        await step_clock(dut)
+        
+        # Now read the output register through the scan chain
+        dut.scan_en.value = 1
+        result_bits = await output_chain(dut, x_out.first, x_out.size)
+        
+        # Convert result bits to integer
+        result = 0
+        for bit in result_bits:
+            result = (result << 1) | bit
+        
+        # Calculate expected result
+        expected = a_val + b_val
+        
+        # Print and check results
+        print(f"Test: {a_val} + {b_val} = {result}, Expected: {expected}")
+        assert result == expected, f"Test failed: {a_val} + {b_val} = {result}, Expected: {expected}"
 
