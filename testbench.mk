89640d32fe8b2543184ab7a33daaf7c024dcfc51
jessica-chann <jchan4@andrew.cmu.edu>
2025-03-29 13:57:16 -0400

    syntax

--- testbench.mk
+++ testbench.mk
@@ -1,6 +1,6 @@
 TOPLEVEL_LANG = verilog
 VERILOG_SOURCES = $(shell pwd)/hidden_fsm/hidden_fsm_out.sv
-TOPLEVEL = adder
+TOPLEVEL = hidden_fsm
 MODULE = ScanChain_starter
 SIM = verilator
 EXTRA_ARGS += --trace -Wno-WIDTHTRUNC -Wno-UNOPTFLAT -Wno-fatal
